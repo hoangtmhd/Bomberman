66fd01de9a937f4a9aa23efff958b13becf31fb0
Sora <hoangtmhd2001@gmail.com>
2020-12-09 00:35:03 +0700

    update Enemy

--- core/src/entities/character/enemy/Enemy.java
+++ core/src/entities/character/enemy/Enemy.java
@@ -1,17 +1,78 @@
 package entities.character.enemy;
 
 import app.management.map.BlockedManagement;
+import app.management.map.Direction;
+import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import entities.character.Character;
 
 public abstract class Enemy extends Character {
-    public Enemy(Sprite sprite, BlockedManagement blockedManagement) {
+    private final Animation<TextureRegion> moveRightAnimation;
+    private final Animation<TextureRegion> moveLeftAnimation;
+
+    private final Animation<TextureRegion> stillRightAnimation;
+    private final Animation<TextureRegion> stillLeftAnimation;
+
+    public Enemy(Sprite sprite, BlockedManagement blockedManagement, String name) {
         super(sprite, blockedManagement);
+
+        // move right.
+        TextureAtlas moveRightTA = new TextureAtlas();
+        moveRightTA.addRegion("0000", prepareRegion(name + "_right1.png"));
+        moveRightTA.addRegion("0001", prepareRegion(name + "_right2.png"));
+        moveRightTA.addRegion("0002", prepareRegion(name + "_right3.png"));
+        moveRightAnimation = new Animation<TextureRegion>(1/5f, moveRightTA.getRegions());
+
+        // move left.
+        TextureAtlas moveLeftTA = new TextureAtlas();
+        moveLeftTA.addRegion("0000", prepareRegion(name + "_left1.png"));
+        moveLeftTA.addRegion("0001", prepareRegion(name + "_left2.png"));
+        moveLeftTA.addRegion("0002", prepareRegion(name + "_left3.png"));
+        moveLeftAnimation = new Animation<TextureRegion>(1/5f, moveLeftTA.getRegions());
+
+        // still right.
+        TextureAtlas stillRightTA = new TextureAtlas();
+        stillRightTA.addRegion("0000", prepareRegion(name + "_right1.png"));
+        stillRightAnimation = new Animation<TextureRegion>(0f, stillRightTA.getRegions());
+
+        // still left
+        TextureAtlas stillLeftTA = new TextureAtlas();
+        stillLeftTA.addRegion("0000", prepareRegion(name + "_left1.png"));
+        stillLeftAnimation = new Animation<TextureRegion>(0f, stillLeftTA.getRegions());
     }
 
     @Override
     public void update(float delta) {
         // change direction.
+        // call changeDirection when want to move, then call stop if blocked.
         super.update(delta);
+    }
+
+    protected void changeDirection(Direction direction) {
+        switch (direction) {
+            case RIGHT:
+            case DOWN:
+                animation = moveRightAnimation;
+            case LEFT:
+            case UP:
+                animation = moveLeftAnimation;
+        }
+        this.direction = direction;
+    }
+
+    protected void stop() {
+        switch (direction) {
+            case RIGHT:
+            case DOWN:
+                animation = stillRightAnimation;
+                break;
+            case LEFT:
+            case UP:
+                animation = stillLeftAnimation;
+                break;
+        }
+        direction = Direction.STAND;
     }
 }
